52e7a5e6a21d119cc1cd95d8c60b583911d7196f
Bendik Austnes <bendia@stud.ntnu.no>
2021-03-11 12:03:00 +0100

    Fix single and multiple cropping. Save cropped RGB for visual inspection

--- roi.py
+++ roi.py
@@ -1,6 +1,7 @@
 import spectral
 import spectral.io.envi as envi
 import cv2
+import PIL
 
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -33,25 +34,61 @@ def select_roi(input_path, output_path, output_path_rgb, rgb_bands):
     img_subset = img.read_subimage(rows=range(r[1], r[1]+r[3]), cols=range(r[0], r[0]+r[2]))
     envi.save_image(
         output_path,
-        img_subset
+        img_subset,
+        force=True # Overwrites already existing file
     )
     print("Successfully cropped hyperspectral datacube to", output_path, "cropped at", r)
 
+    # Crop RGB with ROI
+    output_path_rgb = output_path[:-3] + 'png'
+    cropped_rgb = im[r[1]:r[1]+r[3], r[0]:r[0]+r[2]]
+    cv2.imwrite(output_path_rgb, cropped_rgb)
+    print("Successfully cropped RGB to", output_path_rgb, "cropped at", r)
+
+
+def crop_single(input_path, output_path, output_path_rgb, rgb_bands):
+    select_roi(
+        input_path,
+        output_path,
+        output_path_rgb,
+        rgb_bands
+    )
+
+def crop_multi(input_path, output_path, output_path_rgb, rgb_bands):
+    while(True):
+        print("Specify output filename (\"cancel\" to exit ROI selection):")
+        output_filename = input()
+        output_path = 'data/output/' + output_filename + '.hdr'
+        if output_filename == 'cancel':
+            break
+        select_roi(
+            input_path,
+            output_path,
+            output_path_rgb,
+            rgb_bands
+        )
 
 
 def main(args):
     INPUTPATH = args.filename
-    OUTPUTPATH = 'data/test.hdr'
-    OUTPUT_RGB = 'data/rgb.png'
+    OUTPUTPATH = 'data/output/cropped.hdr'
+    OUTPUT_RGB = 'data/output/RGB.png'
 
     RGBBANDS = [10, 20, 30]
 
-    select_roi(
-        INPUTPATH,
-        OUTPUTPATH,
-        OUTPUT_RGB,
-        RGBBANDS
-    )
+    if args.multi:
+        crop_multi(
+            INPUTPATH,
+            OUTPUTPATH,
+            OUTPUT_RGB,
+            RGBBANDS
+        )
+    else:
+        crop_single(
+            INPUTPATH,
+            OUTPUTPATH,
+            OUTPUT_RGB,
+            RGBBANDS)
     pass
 
 if __name__ == "__main__":
@@ -65,12 +102,13 @@ if __name__ == "__main__":
         help="path to input file",
         type=str,
     )
-    """    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    ) """
+    
+    parser.add_argument(
+        '-m',
+        '--multi',
+        action='store_true',
+        help='activate multiple crops on the same image'
+    )
 
     args = parser.parse_args()
 
