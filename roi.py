8c30c3b0d63b67b745c7f9ae25db85445ca8c76e
Bendik Austnes <bendia@stud.ntnu.no>
2021-03-12 15:56:00 +0100

    Add specific aspect ratio feature, currently in multi mode only and slightly nasty code

--- roi.py
+++ roi.py
@@ -28,6 +28,10 @@ def select_roi(input_path, output_path, output_path_rgb, rgb_bands):
         from_center
     )
 
+    return input_path, output_path, output_path_rgb, rgb_bands, r
+
+def crop_and_save(input_path, output_path, output_path_rgb, rgb_bands, r):
+    img = envi.open(input_path)
     # Crop datacube with ROI
     img_subset = img.read_subimage(rows=range(r[1], r[1]+r[3]), cols=range(r[0], r[0]+r[2]))
     envi.save_image(
@@ -38,32 +42,98 @@ def select_roi(input_path, output_path, output_path_rgb, rgb_bands):
     print("Successfully cropped hyperspectral datacube to", output_path, "cropped at", r)
 
     # Crop RGB with ROI
+    im = cv2.imread(output_path_rgb)
     output_path_rgb = output_path[:-3] + 'png'
     cropped_rgb = im[r[1]:r[1]+r[3], r[0]:r[0]+r[2]]
     cv2.imwrite(output_path_rgb, cropped_rgb)
     print("Successfully cropped RGB to", output_path_rgb, "cropped at", r)
+    
 
 
-def crop_single(input_path, output_path, output_path_rgb, rgb_bands):
-    select_roi(
+def specific_aspect_ratio(args, r):
+    print("Crop r:\t\t\t {}".format(r))
+    aspect_ratio = args.aspect_ratio / 100
+    tolerance = 0.01 # parameter to control output assertion
+
+    r0 = r[0]
+    r1 = r[1]
+    r2 = r[2]
+    r3 = r[3]
+    
+    c1 = r3 # Height of crop in y direction
+    c2 = r2 # Width of crop in x direction
+
+    # From crop height (c1), calculate desired width (c3) for correct AR
+    c3 = int(np.floor(
+        c1 * aspect_ratio
+    ))
+
+    # If crop width is big enough, we're finished since we can make a crop of desired aspect ratio within
+    # the original crop. However, if it isn't we need to calculate a need constraint on the height (c4)
+    # based on available crop width (c2)
+
+    if c3 <= c2:
+        r3 = c1
+        r2 = c3
+    else:
+        c4 = int(np.floor(
+            c2 * (1 / aspect_ratio)
+        ))
+        r3 = c4
+        r2 = c2
+
+    r = (r0, r1, r2, r3)
+    output_ar = r2 / r3
+
+    assert abs(output_ar - aspect_ratio) <= tolerance, "Output aspect ratio is {} but the specified aspect ratio was {} exceeded by tolerance bounds by more than abs({})".format(output_ar, aspect_ratio, tolerance)
+
+    print("AR corrected crop r:\t {}".format(r))
+    print("Desired AR: {} Output AR: {}".format(aspect_ratio, r2 / r3))
+    return r
+    #print("c1: {} c2: {} c3: {}".format(c1, c2, c3))
+    
+
+def crop_single(args, input_path, output_path, output_path_rgb, rgb_bands):
+    
+    input_path, output_path, output_path_rgb, rgb_bands, r = select_roi(
+                                                                        input_path,
+                                                                        output_path,
+                                                                        output_path_rgb,
+                                                                        rgb_bands
+    )
+
+    crop_and_save(
         input_path,
         output_path,
         output_path_rgb,
-        rgb_bands
+        rgb_bands,
+        r
     )
 
-def crop_multi(input_path, output_path, output_path_rgb, rgb_bands):
+def crop_multi(args, input_path, output_path, output_path_rgb, rgb_bands):
+    
     while(True):
         print("Specify output filename (\"cancel\" to exit ROI selection):")
         output_filename = input()
         output_path = 'data/output/' + output_filename + '.hdr'
         if output_filename == 'cancel':
             break
-        select_roi(
+        input_path, output_path, output_path_rgb, rgb_bands, r = select_roi(
+                                                                        input_path,
+                                                                        output_path,
+                                                                        output_path_rgb,
+                                                                        rgb_bands
+        )
+
+        if args.aspect_ratio:
+            r = specific_aspect_ratio(args, r)
+
+        crop_and_save(
             input_path,
             output_path,
             output_path_rgb,
-            rgb_bands
+            rgb_bands,
+            r
         )
 
 
@@ -74,8 +144,12 @@ def main(args):
 
     RGBBANDS = [10, 20, 30]
 
+    if args.aspect_ratio:
+        print(args.aspect_ratio)
+
     if args.multi:
         crop_multi(
+            args,
             INPUTPATH,
             OUTPUTPATH,
             OUTPUT_RGB,
@@ -83,6 +157,7 @@ def main(args):
         )
     else:
         crop_single(
+            args,
             INPUTPATH,
             OUTPUTPATH,
             OUTPUT_RGB,
@@ -106,6 +181,13 @@ if __name__ == "__main__":
         '--multi',
         action='store_true',
         help='activate multiple crops on the same image'
+    )
+
+    parser.add_argument(
+        '-a',
+        '--aspect_ratio',
+        type=int,
+        help='crop aspect ratio in percent (W x H) x 100'
     )
 
     args = parser.parse_args()
